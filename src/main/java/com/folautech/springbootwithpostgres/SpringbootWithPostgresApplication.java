a386d06311e1c8dd751643bf989a4e2cdf130842
Folau Kaveinga <folaukaveinga@gmail.com>
2022-02-22 14:13:21 -0700

    setup

---
+++ src/main/java/com/folautech/springbootwithpostgres/SpringbootWithPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.folautech.springbootwithpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folautech/springbootwithpostgres/SpringbootWithPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.springbootwithpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
