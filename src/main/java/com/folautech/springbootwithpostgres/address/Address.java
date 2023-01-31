594699421cc40a9e360c4b6eade4537048400ba6
folaulau <folaukaveinga@gmail.com>
2023-01-30 17:59:59 -0700

    updated to spring 3

--- src/main/java/com/folautech/springbootwithpostgres/address/Address.java
+++ src/main/java/com/folautech/springbootwithpostgres/address/Address.java
@@ -3,15 +3,15 @@ package com.folautech.springbootwithpostgres.address;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.ResultCheckStyle;
--- src/main/java/com/folautech/springbootwithpostgres/user/User.java
+++ src/main/java/com/folautech/springbootwithpostgres/user/User.java
@@ -5,14 +5,14 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
@@ -62,7 +62,6 @@ public class User implements Serializable {
     @Column(name = "email", unique = true)
     private String           email;
 
-    @Type(type = "true_false")
     @Column(name = "deleted", nullable = false)
     private boolean          deleted;
 
